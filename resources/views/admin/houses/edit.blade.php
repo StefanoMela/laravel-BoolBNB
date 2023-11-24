4f48f2da944aeaaa96926c9b51a4cc22229e813a
AlessioGugielmino <guglielmino.alessio@virgilio.it>
2023-11-24 12:56:08 +0100

    add_essential

--- resources/views/admin/houses/edit.blade.php
+++ resources/views/admin/houses/edit.blade.php
@@ -2,6 +2,6 @@
 
 @section('content')
 <section class="container">
-  @include('admin.houses.partials.form', ["title" => "Edit House", "route" => 'admin.houses.update', 'idForm'=>'create-form', 'methodRoute' => 'PATCH', 'btnClass' => 'create-btn'])
+  @include('admin.houses.partials.form', ["title" => "Edit House","essential" => "", "route" => 'admin.houses.update', 'idForm'=>'create-form', 'methodRoute' => 'PATCH', 'btnClass' => 'create-btn'])
 </section>
 @endsection
--- resources/views/admin/houses/partials/form.blade.php
+++ resources/views/admin/houses/partials/form.blade.php
@@ -10,17 +10,9 @@
         <h2 class="text-center mb-2">{{$title}}</h2>
       </div>
       <div class="card-body">
-        {{-- <div class="form-outline w-25 mb-3">
-            <label for="ISBN" class="form-label">ISBN</label>
-            <input type="text" class="form-control @error('ISBN') is-invalid @enderror" maxlength="13" id="ISBN" name="ISBN" value="{{ old('ISBN', $house->ISBN) }}">
-            @error('ISBN')
-            <div class="invalid-feedback">
-                {{ $message }}
-            </div>
-            @enderror    
-        </div> --}}
+   
         <div class="mb-3 w-50">
-            <label for="title" class="form-label">Titolo</label>
+            <label for="title" class="form-label">Titolo {{$essential}}</label>
             <input type="text" class="form-control @error('title') is-invalid @enderror"  maxlength="200" id="title" name="title" value="{{ old('title',$house->title) }}">           
             @error('title')
             <div class="invalid-feedback">
@@ -31,7 +23,7 @@
         </div>
 
         <div class="form-outline mb-3">
-            <label for="address" class="form-label">Indirizzo</label>
+            <label for="address" class="form-label">Indirizzo {{$essential}}</label>
             <input type="text" class="form-control @error('address') is-invalid @enderror" id="address" name="address" value="{{ old('address',$house->address) }}">           
             @error('address')
             <div class="invalid-feedback">
@@ -41,7 +33,7 @@
 
         </div>
         <div class="form-outline w-25 mb-3">
-            <label for="cover_image" class="form-label">Foto della casa</label>
+            <label for="cover_image" class="form-label">Foto della casa {{$essential}}</label>
             <input type="file" class="form-control @error('cover_image') is-invalid @enderror" id="cover_image" name="cover_image" value="{{old('cover_image', $house->cover_image)}}">           
             @error('cover_image')
             <div class="invalid-feedback">
@@ -50,7 +42,7 @@
             @enderror    
         </div>
         <div class="mb-3">
-            <label for="description" class="form-label">Descrizione</label>
+            <label for="description" class="form-label">Descrizione {{$essential}}</label>
             <textarea type="text" class="form-control @error('description') is-invalid @enderror" id="description" name="description" value="" cols="30" rows="5">{{ old('description',$house->description) }}</textarea>
             @error('description')
             <div class="invalid-feedback">
@@ -62,8 +54,8 @@
         <div class="row">
             <div class="col-4">
                 <div class="mb-3">
-                    <label for="rooms" class="form-label">Stanze</label>
-                    <input type="number" class="form-control @error('rooms') is-invalid @enderror" id="rooms" name="rooms" value="{{  old('rooms', $house->rooms) }}">
+                    <label for="rooms" class="form-label">Stanze {{$essential}}</label>
+                    <input type="number" min="1" class="form-control @error('rooms') is-invalid @enderror" id="rooms" name="rooms" value="{{  old('rooms', $house->rooms) }}">
                     @error('rooms')
                     <div class="invalid-feedback">
                         {{ $message }}
@@ -74,8 +66,8 @@
             </div>
             <div class="col-4">
                 <div class="mb-3">
-                    <label for="sq_meters" class="form-label">Metri quadri</label>
-                    <input type="number" class="form-control @error('sq_meters') is-invalid @enderror" id="sq_meters" name="sq_meters" value="{{ old('sq_meters', $house->sq_meters) }}">
+                    <label for="sq_meters" class="form-label">Metri quadri {{$essential}}</label>
+                    <input type="number"min="10" step="5" class="form-control @error('sq_meters') is-invalid @enderror" id="sq_meters" name="sq_meters" value="{{ old('sq_meters', $house->sq_meters) }}">
                     @error('sq_meters')
                     <div class="invalid-feedback">
                         {{ $message }}
@@ -86,8 +78,8 @@
             </div>
             <div class="col-4">
                 <div class="mb-3">
-                    <label for="beds" class="form-label">Letti</label>
-                    <input type="number" class="form-control @error('beds') is-invalid @enderror" id="beds" name="beds" value="{{  old('beds', $house->beds) }}">
+                    <label for="beds" class="form-label">Letti {{$essential}}</label>
+                    <input type="number"min="1" class="form-control @error('beds') is-invalid @enderror" id="beds" name="beds" value="{{  old('beds', $house->beds) }}">
                     @error('beds')
                     <div class="invalid-feedback">
                         {{ $message }}
@@ -98,8 +90,8 @@
 
             <div class="col-4">
                 <div class="mb-3">
-                    <label for="bathrooms" class="form-label">Bagni</label>
-                    <input type="number" class="form-control @error('bathrooms') is-invalid @enderror" id="bathrooms" name="bathrooms" value="{{  old('bathrooms', $house->bathrooms) }}">
+                    <label for="bathrooms" class="form-label">Bagni {{$essential}}</label>
+                    <input type="number" min="1" class="form-control @error('bathrooms') is-invalid @enderror" id="bathrooms" name="bathrooms" value="{{  old('bathrooms', $house->bathrooms) }}">
                     @error('bathrooms')
                     <div class="invalid-feedback">
                         {{ $message }}
@@ -110,7 +102,7 @@
 
         </div>
         <div>
-            <label for="extras" class="form-label">Servizi aggiuntivi</label>
+            <label for="extras" class="form-label">Servizi aggiuntivi {{$essential}}</label>
             <div class="form-check @error('extras') is-invalid @enderror">
                 <div class="row mt-3">
                     @foreach ($extras as $extra)
@@ -138,6 +130,11 @@
         <a href="{{ route('admin.houses.index')}}" class="btn btn-dark"><i class="fa-solid fa-arrow-left"></i>&nbsp;Cancel</a>
         <button type="submit" class="btn btn-success {{$btnClass}}"><i class="fa-solid fa-save"></i>&nbsp;Submit</button>
       </div>
+    </div>
+
+    <div>
+        <p class="fs-6 fst-italic text-secondary">Sono contrassegnati con * i dati obbligatori.
+        </p>
     </div>
 </form>
 {{-- 
