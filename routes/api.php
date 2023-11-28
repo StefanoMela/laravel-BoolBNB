9dbd749a20fde8f5cfc0b4c885b3b8c1fef5945e
StefanoMela <stefano.mela25@gmail.com>
2023-11-28 16:35:30 +0100

    add extras controller

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Api\FeaturedHouseController;
 use App\Http\Controllers\Api\SearchController;
+use App\Http\Controllers\Api\ExtrasController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -23,3 +24,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::apiResource('houses', FeaturedHouseController::class);
 Route::apiResource('search', SearchController::class);
+Route::apiResource('extras', ExtrasController::class);
