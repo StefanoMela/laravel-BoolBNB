e7c7a24af9daa9a24a860a46ee2f40421c37cadb
StefanoMela <stefano.mela25@gmail.com>
2023-12-09 11:38:46 +0100

    fix-preview// small-gfx-fix

--- app/Http/Controllers/HouseController.php
+++ app/Http/Controllers/HouseController.php
@@ -147,6 +147,7 @@ class HouseController extends Controller
             return view("admin.houses.edit", compact("house", "extras", "extra_house"));
 
         }
+
         return redirect()->route('admin.houses.index');
     }
 
@@ -166,6 +167,18 @@ class HouseController extends Controller
                 Storage::delete($house->cover_image);
             }
             $data['cover_image'] = Storage::put('uploads/houses/cover_image', $data['cover_image']);
+        };
+
+
+        if ($request->hasFile('image')) {
+            $images = $request->file('image');
+            foreach ($images as $image) {
+                $gallery = new Gallery;
+                $gallery->house_id = $house->id;
+                $path = $image->store('uploads/houses/gallery_images');
+                $gallery->fill(['image' => $path]);
+                $gallery->save();
+            }
         };
 
         $house->fill($data);
--- resources/views/admin/houses/create.blade.php
+++ resources/views/admin/houses/create.blade.php
@@ -1,22 +1,13 @@
 @extends('layouts.app')
 
 @section('content')
-<section class="container">
-  @include('admin.houses.partials.form', ["title" => "Add New House","essential" => "*", "route" => 'admin.houses.store', 'idForm'=>'create-form', 'methodRoute' => 'POST', 'btnClass' => 'create-btn'])
-</section>
+@include('admin.houses.partials.form', ["title" => "Add New House","essential" => "*", "route" =>
+'admin.houses.store', 'idForm'=>'create-form', 'methodRoute' => 'POST', 'btnClass' => 'create-btn'])
 @endsection
 
 
 @section('scripts')
 <script>
-    const previewImg = document.getElementById('cover_image_preview');
-    const inputFile = document.getElementById('cover_image');
-
-   
-    inputFile.addEventListener('change', function() {
-        const [file] = this.files
-        previewImg.src= URL.createObjectURL(file);
-    })
 
 </script>
 @endsection
